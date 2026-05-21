7d3fd9ece47aa89ce81d520ebe6dc0edb4637401
Netology test <gulaevmihail112@gmail.com>
2026-05-21 21:21:33 +0500

    Commit_1

--- PythonProject4/OOP_Learn_homework_1.py
+++ PythonProject4/OOP_Learn_homework_1.py
@@ -14,6 +14,14 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+# Дочерний класс для лекторов
+class Lecturer(Mentor):
+    pass
+
+
+# Дочерний класс для проверяющих
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -24,14 +32,24 @@ class Mentor:
             return 'Ошибка'
 
 
+#Проверка из задания №1
+
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+
+print(isinstance(lecturer, Mentor))  # True
+print(isinstance(reviewer, Mentor))  # True
+print(lecturer.courses_attached)  # []
+print(reviewer.courses_attached)  # []
+
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 9)
 
-print(best_student.grades)
+print(best_student.grades)  # {'Python': [10, 9]}
